f51021d708dce22496024855b312b864bf76c7fe
tfxdx <yunumendozaperez@gmail.com>
2026-03-28 19:50:36 -0500

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -11,3 +11,10 @@ import android.os.Build.VERSION as AndroidVersion;
 import android.os.Build.VERSION_CODES as AndroidVersionCode;
 import android.os.BatteryManager as AndroidBatteryManager;
 #end
+#if mobile
+import mobile.flixel.FlxHitbox;
+import mobile.flixel.FlxVirtualPad;
+import mobile.flixel.input.FlxMobileInputID;
+import mobile.backend.SUtil;
+import mobile.backend.MobileScaleMode;
+#end
